b65ac565f9dc49d15b6e5dc0c18a4287bcf5770c
Hardik <beladiyahardik755@gmail.com>
2022-01-01 10:39:42 +0530

    Setup CSS and ANT design

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,9 +1,10 @@
+import { Button, DatePicker } from "antd";
 import React from "react";
 
 const Index = () => {
   return (
     <div>
-      <div className="mainScreen">Hello world!</div>
+      <div className="mainScreen d-flex x-center y-center"><h1 className="text-white">Hello world!</h1></div>
     </div>
   );
 };
